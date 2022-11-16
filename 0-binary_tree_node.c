818019c97a1ab3a496fdf508a8e02a0bea421de1
callmeoli <olikorma@gmail.com>
2022-11-16 17:27:40 +0300

    0-node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 /**
  * binary_tree_node - creat node
@@ -11,7 +10,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = (binary_tree_t *)  malloc(sizeof(binary_tree_t));
+	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 	{
 		return (NULL);
