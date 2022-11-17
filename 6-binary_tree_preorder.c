125b399c17fef721863f2cba56e624d7d1114650
callmeoli <olikorma@gmail.com>
2022-11-17 17:29:48 +0300

    6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_preorder - delete entire node
+* @tree: root node
+* @func: function pointer
+* Return: None
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+	{
+		return;
+	}
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
