8901e7a192a29adcbb43ec71cd359ff7789be5d5
callmeoli <olikorma@gmail.com>
2022-11-17 16:11:43 +0300

    5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_root - check if binary node is root
+* @node: root node
+* Return: None
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+	{
+		return (0);
+	}
+	if (!node->parent)
+	{
+		return (1);
+	}
+
+	return (0);
+}
